3f5fd365818c9656bc29e99a0d84450f64f143df
User Umid <umidjoninagamov@yandex.ru>
2024-12-25 06:05:55 +0500

    Second commit

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,12 +1,17 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.List;
 
 public interface UserStorage {
     User addUser(User user);
+
     User updateUser(User user);
+
     void deleteUser(int id);
+
     User getUserById(int id);
+
     List<User> getAllUsers();
 }
