9c48a33e135e94fd79d8037982d5d43089865375
User Umid <umidjoninagamov@yandex.ru>
2024-12-26 20:11:07 +0500

    Second commit

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ValidationException;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
